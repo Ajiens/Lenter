d71e65ad5ebb2c93f39a553474012a2f5f379a38
alwantakaditama@gmail.com <alwantakaditama@gmail.com>
2025-01-03 15:21:15 +0700

    Update gradle version to 8.5 for Java JDK 21.0

---
+++ android/app/src/main/kotlin/com/example/apap_medika/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apap_medika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/lenter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lenter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
